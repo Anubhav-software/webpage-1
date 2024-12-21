39de00211018a9a3dcddaa1c86e7aed35a3bd93e
Anubhav-rawat <anubhav@anayasoftwares.in>
2024-12-21 16:26:10 +0530

    hero-done

--- src/components/manual/Navbar/Navbar.jsx
+++ src/components/manual/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { FaLeaf } from "react-icons/fa";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { MdMenu } from "react-icons/md";
 import ResponsiveMenu from "./ResponsiveMenu";
-
+import { motion } from "framer-motion";
 const NavbarMenu = [
   {
     id: 1,
@@ -37,7 +37,12 @@ const Navbar = () => {
   return (
     <>
       <nav>
-        <div className="container flex items-center justify-between py-4 md:pt-4">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className="container flex items-center justify-between py-4 md:pt-4"
+        >
           {/* Logo Section */}
           <div className="text-2xl flex items-center font-bold gap-2 uppercase">
             <p className="text-primary">Fruit</p>
@@ -65,10 +70,10 @@ const Navbar = () => {
           </div>
 
           {/*  Mobile Hamburger Menu Section */}
-          <div className="md:hidden" onClick={()=>setOpen(!open)}>
+          <div className="md:hidden" onClick={() => setOpen(!open)}>
             <MdMenu className="text-4xl" />
           </div>
-        </div>
+        </motion.div>
       </nav>
 
       {/* Mobile Menu Section */}
